c47fc9f2f59c1316b139ee2332e348c1e34b10c2
alexanderbockelman <alexanderbockelman@users.noreply.github.com>
2018-09-12 08:33:30 -0500

    added 1e and 1f

---
+++ program1e.py
@@ -0,0 +1,12 @@
+# By submitting this assignment, I agree to the following:
+# "Aggies do not lie, cheat, or steal, or tolerate those who do"
+# "I have not given or received any unauthorized aid on this assignment"
+#
+# Name: ALEXANDER BOCKELMAN
+# Section: 211
+# Assignment: Lab 3a-Program 1e
+# Date: 12 September 2018
+
+v_1 = float(input("Enter Velocity in Miles per Hour: "))
+v_2 = v_1*(44.704/100)
+print("Velocity in Meters per Second: ", v_2)
---
+++ program1f.py
@@ -0,0 +1,12 @@
+# By submitting this assignment, I agree to the following:
+# "Aggies do not lie, cheat, or steal, or tolerate those who do"
+# "I have not given or received any unauthorized aid on this assignment"
+#
+# Name: ALEXANDER BOCKELMAN
+# Section: 211
+# Assignment: Lab 3a-Program 1f
+# Date: 12 September 2018
+
+Temp_f = float(input("Enter Temperature in Fahrenheit: "))
+Temp_c = (Temp_f - 32)*(5/9)
+print("Temperature in Celsius: ", Temp_c)
