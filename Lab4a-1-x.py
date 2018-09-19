c1ab08ec8b2db74011009082f084dba1a0366968
PraiseTheGoldenHoodie <34354911+PraiseTheGoldenHoodie@users.noreply.github.com>
2018-09-19 08:07:39 -0500

    started lab4a-1

--- Lab4a-1-x.py
+++ Lab4a-1-x.py
@@ -1,13 +1,20 @@
 # By submitting this assignment, all team members agree to the following:
 #  “Aggies do not lie, cheat, or steal, or tolerate those who do”
 #  “I have not given or received any unauthorized aid on this assignment”
-# 
+#
 # Names: 	Alexander Bockelman
 #           Jose Carrillo
 # 	 		Patrick Chai
 # 	 		Jackson Sanders
 # Section:		211
-# Assignment:	THE ASSIGNMENT NUMBER 
-# Date:		DAY MONTH 2018
+# Assignment:	4a-1-[x]
+# Date:		19 9 2018
 
-
+a = 1/7
+print(a)
+b = a*7
+print(b)
+c = 2*a
+d = 5*a
+e = c+d
+print(e)
