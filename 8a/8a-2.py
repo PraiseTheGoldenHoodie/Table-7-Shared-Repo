42ce3f93de11d6dda8137322824facc0a7d102c9
PraiseTheGoldenHoodie <34354911+PraiseTheGoldenHoodie@users.noreply.github.com>
2018-10-15 10:10:18 -0500

    group work on 8a on 10/15/18

---
+++ 8a/8a-2.py
@@ -0,0 +1,14 @@
+# By submitting this assignment, all team members agree to the following:
+#  “Aggies do not lie, cheat, or steal, or tolerate those who do”
+#  “I have not given or received any unauthorized aid on this assignment”
+# 
+# Names: 	Alexander Bockelman
+#           Jose Carrillo
+# 	 		Patrick Chai
+# 	 		Jackson Sanders
+# Section:		211
+# Assignment:	8a-2
+# Date:		27 10 2018
+
+# TODO: write each section as an individual assignment; then combine code from each
+# into this .py file. We DO submit each individual assignment's .py file  as well.
