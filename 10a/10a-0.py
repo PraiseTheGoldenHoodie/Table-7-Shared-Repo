57cfc7831d03204037af4c73565c2b1d27fa94e5
PraiseTheGoldenHoodie <34354911+PraiseTheGoldenHoodie@users.noreply.github.com>
2018-10-29 08:53:12 -0500

    created empty files for 10a

---
+++ 10a/10a-0.py
@@ -0,0 +1,19 @@
+# By submitting this assignment, I agree to the following:
+# "Aggies do not lie, cheat, or steal, or tolerate those who do"
+# "I have not given or received any unauthorized aid on this assignment"
+#
+# Name: Alexander Bockelman
+#       Jackson Sanders
+#       Patrick Chai
+#       Jose Carrillo
+#
+# Section: 211
+# Assignment: Lab 10a- Activity 0
+# Date: DATE DATE 2018
+try:
+    import numpy
+    import matplotlib
+except ImportError:
+    print("Packages cannot be imported!!!! Fix this!!!!")
+else:
+    print("Numpy and Matplotlib packages are installed.")
---
+++ 10a/10a-1.py
@@ -0,0 +1,13 @@
+# By submitting this assignment, I agree to the following:
+# "Aggies do not lie, cheat, or steal, or tolerate those who do"
+# "I have not given or received any unauthorized aid on this assignment"
+#
+# Name: Alexander Bockelman
+#       Jackson Sanders
+#       Patrick Chai
+#       Jose Carrillo
+#
+# Section: 211
+# Assignment: Lab 10a- Activity 1
+# Date: DATE DATE 2018
+import numpy
