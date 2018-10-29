82ed402d8ba350e918d853c5a2bd217eb1e24109
PraiseTheGoldenHoodie <34354911+PraiseTheGoldenHoodie@users.noreply.github.com>
2018-10-29 08:15:42 -0500

    finished 9a

--- 9a/9a.py
+++ 9a/9a.py
@@ -13,7 +13,22 @@
 input_path = "input.txt"
 output_path = "output.txt"
 
+vowels = ['a', 'e', 'i', 'o', 'u', 'y']                                                                                 # Assigns values
 with open(input_path, 'r') as fileName:
     english_lines = fileName.readlines()
 for line in english_lines:
-    print(line.split())
+    #print(line.split())
+    for word in line.split():
+        first_letter = word[0:1]                                                                                            # Assigns the first character from the word string to variable
+        is_vowel = False                                                                                                        # Pretty much a boolean trigger to let you know it's not a vowel
+        for i in vowels :
+            if first_letter == i :                                                                                          # If the first letter is a vowel
+                print(word+'yay', end=' ')
+                is_vowel = True                                                                                             # Redefines the boolean and stops the program
+                break
+
+        while not is_vowel :                                                                                                # Starts with consonant
+            print(word[1:]+first_letter+"ay", end=' ')
+            break
+    print()
+
