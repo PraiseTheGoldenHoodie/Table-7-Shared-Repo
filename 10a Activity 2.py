78b841ca8699d4c7a62dbeb0bcec3ef3c5700e53
patchai <43209356+patchai@users.noreply.github.com>
2018-10-31 09:46:37 -0500

    Add files via upload

---
+++ 10a Activity 2.py
@@ -0,0 +1,8 @@
+import matplotlib.pyplot as plt
+from math import*
+
+plt.plot([1, 2, 3, 4], [1,2, 3, 4] , [1, 2, 3, 4])
+plt.show()
+
+plt.plot([cos(2) ,sin(888) ,tan(0.665) ,10 ,99999] , [0.5 ,5 , 50 , 500 , 5000])
+plt.show()
